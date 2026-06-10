ef0f18a067c27de1b7798ba9f1026623d6be1eaf
Ali Riza Keles <alirizakeles66@gmail.com>
2026-06-10 10:00:10 +0200

    feat: add Terraform outputs for container registry

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = azurerm_resource_group.main.location
 }
+
+output "container_registry_name" {
+  value = azurerm_container_registry.main.name
+}
+
+output "container_registry_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
