04109c9dc6babd8dd348cf55500c73c112e15b36
Ali Riza Keles <alirizakeles66@gmail.com>
2026-09-12 21:10:41 +0200

    fix: correct provider configuration and lock dependencies

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,6 @@
 terraform {
   required_version = ">= 1.5, < 2.0"
 
-  # Configure the Azure Resource Manager provider
-  provider "azurerm" {
-    features {}
-  }
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -14,6 +9,7 @@ terraform {
   }
 }
 
+# Configure the Azure Resource Manager provider
 provider "azurerm" {
   features {}
 }
